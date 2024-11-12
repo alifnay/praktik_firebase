54f2308c5f92e9deca3aa25d9bf6630c76a0a90e
atzulf <atzulfikar45@gmail.com>
2024-11-12 07:48:23 +0700

    textview register to login done

--- app/src/main/java/com/example/firebaseapp/register.java
+++ app/src/main/java/com/example/firebaseapp/register.java
@@ -1,6 +1,9 @@
 package com.example.firebaseapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,16 @@ public class register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.register), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        TextView textToLogin = findViewById(R.id.text_to_login);
+
+        // Tambahkan OnClickListener pada text_to_login
+        textToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Pindah ke LoginActivity
+                Intent intent = new Intent(register.this, login.class);
+                startActivity(intent);
+            }
         });
     }
 }
